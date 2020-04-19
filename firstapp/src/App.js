c80711b0f6a15da3ed8bea0be6414df8ac87afb5
gunjanbansal9512 <gunjanbansalcitech@gmail.com>
2020-04-19 20:12:51 +0530

    first app

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,20 +1,38 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import Demo from './Demo';
 
 class App extends React.Component
 {
-constructor(props)
-{
-  super(props);
   
+    state = {
+      person: [
+       { name:"Gunjan",age : 25},
+       {
+         name:"Harsh",age : 30
+       }
+      ]
+    }  
+switchNameHandler = ()=>
+{
+  this.setState({person: [
+    { name:"Gunjan Bansal",age : 25},
+    {
+      name:"Harsh",age : 30
+    }
+   ]})
+
 }
   render()
   {
     return(
       <div>
         <h1>Hiii</h1>
+        <button onClick={this.switchNameHandler}>
+          Switch Name
+        </button>
+        <Demo name={this.state.person[0].name}></Demo>
+        <Demo name={this.state.person[1].name}></Demo>
       </div>
     );
   }
